f306bc7e66a7a1ddbf5370ddeaf6bad3a596853a
David Hirtz <david.hirtz@me.com>
2019-02-04 15:14:12 +0100

    PSR-2 formatting

--- Timeago.php
+++ Timeago.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Timeago.php
  * @author David Hirtz
  * @link https://www.davidhirtz.com
  */
+
 namespace davidhirtz\yii2\timeago;
+
 use Yii;
 use yii\helpers\Html;
 
@@ -14,32 +15,30 @@ use yii\helpers\Html;
  */
 class Timeago
 {
-	/**
-	 * @var bool
-	 */
-	private static $isRegistered=false;
+    /**
+     * @var bool
+     */
+    private static $isRegistered = false;
 
-	/**
-	 * Renders time tag.
-	 *
-	 * @param int|string|\DateTime $time
-	 * @param array $options
-	 * @return string
-	 */
-	public static function tag($time, $options=[])
-	{
-		if($time)
-		{
-			if(!self::$isRegistered)
-			{
-				TimeagoAsset::register(Yii::$app->getView());
-				self::$isRegistered=true;
-			}
+    /**
+     * Renders time tag.
+     *
+     * @param int|string|\DateTime $time
+     * @param array $options
+     * @return string
+     */
+    public static function tag($time, $options = [])
+    {
+        if ($time) {
+            if (!static::$isRegistered) {
+                TimeagoAsset::register(Yii::$app->getView());
+                self::$isRegistered = true;
+            }
 
-			Html::addCssClass($options, 'timeago');
-			$options['datetime']=Yii::$app->formatter->asDatetime($time, "php:c");
+            Html::addCssClass($options, 'timeago');
+            $options['datetime'] = Yii::$app->getFormatter()->asDatetime($time, "php:c");
 
-			return Html::tag('time', Yii::$app->formatter->asDatetime($time), $options);
-		}
-	}
+            return Html::tag('time', Yii::$app->getFormatter()->asDatetime($time), $options);
+        }
+    }
 }
